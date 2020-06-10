63dbc106401cf059514003b06ca38f444c4605a7
Channa Ly <channa.info@gmail.com>
2020-06-10 08:37:18 +0700

    Generate the company model

--- transit/admin.py
+++ transit/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from modeltrans.admin import ActiveLanguageMixin
+from .models import Company
 
+class CompanyAdmin(ActiveLanguageMixin, admin.ModelAdmin):
+    pass
 # Register your models here.
+
+admin.site.register(Company, CompanyAdmin)
--- transit/models.py
+++ transit/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.postgres.indexes import GinIndex
+
+from modeltrans.fields import TranslationField
+
 
 # Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    short_description = models.CharField(max_length=255)
+    description = models.TextField(max_length=60000)
+    about = models.CharField(max_length=255)
+
+    slug = models.CharField(max_length=255)
+
+    i18n = TranslationField(fields=("name", "short_description", "description", "about"))
+
+    class Meta:
+        indexes = [GinIndex(fields=["i18n"]), ]
