71785400b1a96d08b19ae5c06322d99c7d1da771
Channa Ly <channa.info@gmail.com>
2020-06-21 23:04:26 +0700

    Add test to create user profile when create a user with default profile pic

--- transit/tests.py
+++ transit/tests.py
@@ -1,22 +1,36 @@
 from django.test import TestCase
 
 from transit.models import Company
+from transit.models import UserProfile
+from django.contrib.auth.models import User
+
+
 from django.conf import settings
 # Create your tests here.
 
 class CompanyTestCase(TestCase):
     def test_name_locale(self):
 
-        company = Company.objects.create(name='The blue sea', name_kh='សមុទ្រខៀវ', name_zh='蓝色的大海')
+        company = Company.objects.create(name='The blue sea', name_km='សមុទ្រខៀវ', name_zh_hans='蓝色的大海')
         company.save()
 
-        json_i18n = {'name_kh': 'សមុទ្រខៀវ', 'name_zh': '蓝色的大海'}
+        json_i18n = {'name_km': 'សមុទ្រខៀវ', 'name_zh_hans': '蓝色的大海'}
         self.assertEqual(settings.LANGUAGE_CODE, 'en')
         self.assertEqual(company.i18n, json_i18n)
         
         self.assertEqual(company.name, 'The blue sea')
         self.assertEqual(company.name_en, 'The blue sea')
 
-        self.assertEqual(company.name_kh, 'សមុទ្រខៀវ')
-        self.assertEqual(company.name_zh, '蓝色的大海')
-        self.assertEqual(company.name_fr, None)
+        self.assertEqual(company.name_km, 'សមុទ្រខៀវ')
+        self.assertEqual(company.name_zh_hans, '蓝色的大海')
+        self.assertEqual(company.name_vi, None)
+
+
+class UserTestCase(TestCase):
+    def test_create_user_profile_when_create_user(self):
+        user = User.objects.create(username='joeann', password='cienfuegos')
+        self.assertEqual(user.username, 'joeann')
+        self.assertIsNotNone(user.id)
+        self.assertEqual(user.user_profile.profile_pic.url, '/uploads/default-pic.png')
+        
+
